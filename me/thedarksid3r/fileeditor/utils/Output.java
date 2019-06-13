506b033818028b6fb3e43d60295720acdd851824
[ÐJ]Medic <EndNation@users.noreply.github.com>
2019-06-13 20:27:20 +0800

    Create Output.java

---
+++ me/thedarksid3r/fileeditor/utils/Output.java
@@ -0,0 +1,19 @@
+package me.thedarksid3r.fileeditor.utils;
+
+public class Output 
+{
+	public void log(String msg)
+	{
+		System.out.println(msg);
+	}
+	
+	public void log(int number)
+	{
+		System.out.println(number);
+	}
+	
+	public void log(boolean bool)
+	{
+		System.out.println(bool);
+	}
+}
